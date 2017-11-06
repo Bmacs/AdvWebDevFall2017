853133f0ece35c8195f538b85b43097b3108b426
Brendan McNamara <Brendancmcnamara@gmail.com>
2017-11-06 15:51:20 -0500

    added require db

--- NODE-DEVKIT/AdvWebDevFall2017/week4/lab/app.js
+++ NODE-DEVKIT/AdvWebDevFall2017/week4/lab/app.js
@@ -5,11 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+require('./db');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-require('./db');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,24 +29,22 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+    // render the error page
     res.status(err.status || 500);
     res.render('error');
 });
 
 require('express-debug')(app);
-
-module.exports = app;
 
 module.exports = app;
